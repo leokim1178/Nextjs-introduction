8df8a35d36e7cf7683ba635f5542186bbd2764a9
LeoKim <leo3179@naver.com>
2023-03-05 19:02:34 +0900

    ✨ FEAT /  #2.2 fetch Data and Hide env

--- components/Leo.js
+++ components/Leo.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 
 export default function Leo({ title }) {
+  const pageTitle = `${title} | Next Movies`;
   return (
     <Head>
-      <title>{title} | Next Movies</title>
+      <title>{pageTitle}</title>
     </Head>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,48 @@
+import { useEffect, useState } from "react";
 import Leo from "../components/leo";
 
 export default function Home() {
-  return (
-    <div>
-      <Leo title="Home" />
+  const [movies, setMovies] = useState();
 
-      <h1 className="active">Hello</h1>
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`/api/movies`);
+      const { results } = await res.json();
+      setMovies(results);
+    })();
+  }, []);
+
+  return (
+    <div className="container">
+      <Leo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
